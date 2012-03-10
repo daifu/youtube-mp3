47a9ca64d31421101d53e3bb93cd94da84c39c68
dejay <chipsolderer@gmail.com>
2012-03-10 11:55:57 -0700

    update

--- brew.youtube-mp3.rb
+++ brew.youtube-mp3.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class YoutubeMp < Formula
+class BrewYoutubeMp3 < Formula
   url 'https://raw.github.com/dejay/youtube-mp3/master/youtube-mp3'
   homepage 'http://dejay.github.com'
   md5 '0b631b4fa33cc1e104665ba1871232f8'
