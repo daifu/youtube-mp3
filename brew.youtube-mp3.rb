f13392d9e202ba429c60b68bc0c5cc8053ef8b8e
dejay <chipsolderer@gmail.com>
2012-03-10 11:41:31 -0700

    now you can install with brew

--- brew.youtube-mp3.rb
+++ brew.youtube-mp3.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class youtubemp3 < Formula
+class youtubemp3 < ScriptFileFormula
   url 'https://raw.github.com/dejay/youtube-mp3/master/youtube-mp3'
   homepage 'http://dejay.github.com/'
 
